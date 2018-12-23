acafc1270c8061e79ce5f347f9f69d3311973cab
vladimir86ns <vladimir86ns@gmail.com>
2018-12-23 21:10:02 +0100

    feature - removed logo and profile pictures

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -63,20 +63,12 @@ class Sidebar extends Component {
 					style={{ backgroundImage: enableSidebarBackgroundImage ? `url(${selectedSidebarImage})` : 'none' }}
 				>
 					<div className={classNames("rct-sidebar-content", { "sidebar-overlay-dark": isDarkSidenav, 'sidebar-overlay-light': !isDarkSidenav })}>
-						<div className="site-logo">
-							<Link to="/" className="logo-mini">
-								<img src={require('Assets/img/appLogo.png')} className="mr-15" alt="site logo" width="35" height="35" />
-							</Link>
-							<Link to="/" className="logo-normal">
-								<img src={require('Assets/img/appLogoText.png')} className="img-fluid" alt="site-logo" width="67" height="17" />
-							</Link>
-						</div>
 						<div className="rct-sidebar-wrap">
 							<Scrollbars
 								className="rct-scroll"
 								autoHide
 								autoHideDuration={100}
-								style={{ height: 'calc(100vh - 60px)' }}
+								style={{ height: 'calc(120vh - 60px)' }}
 							>
 								<UserBlock />
 								{!agencySidebar ?
